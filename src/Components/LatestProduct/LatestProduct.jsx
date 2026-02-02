6781e8965fd33d46918576fde3fa64b9b2830858
Tariful Hasan <tarif305@gmail.com>
2026-02-02 19:45:01 +0600

    production workflow added

---
+++ src/Components/LatestProduct/LatestProduct.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import ProductCard from "./ProductCard";
+
+const LatestProduct = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/latestProduct")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((err) => err);
+  }, []);
+
+  return (
+    <>
+      <div>
+        <h1 className="font-bold text-4xl  text-[#384bb4] mt-15" >Latest Products</h1>
+        <p  className="text-xs mt-2">
+          Discover our high-quality garments ready for bulk production. <br />
+          Customizable to your brand's needs.
+        </p>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 p-10 gap-5">
+        {products.map((product) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
+      </div>
+      <div>
+        <button className="font-semibold text-xl text-[#384bb4] btn">View All Products →</button>
+      </div>
+    </>
+  );
+};
+
+export default LatestProduct;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Banner from "../Components/HeroSection/Banner"
+import LatestProduct from '../Components/LatestProduct/LatestProduct';
+import ProdWorkFlow from '../Components/WorkFlow/ProdWorkFlow';
 
 const Home = () => {
     return (
         <div>
             <div className='mt-10'>
                 <Banner/>
+            </div>
+            <div>
+                <LatestProduct/>
+            </div>
+            <div>
+                <ProdWorkFlow/>
             </div>
         </div>
     );
