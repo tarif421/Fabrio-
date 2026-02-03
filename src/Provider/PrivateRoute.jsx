2f97679088ca173708f237ba6dcdc500c116f304
Tariful Hasan <tarif305@gmail.com>
2026-02-03 20:44:52 +0600

    private route added

---
+++ src/Provider/PrivateRoute.jsx
@@ -0,0 +1,24 @@
+import React, { use } from "react";
+
+import { Navigate, useLocation } from "react-router";
+import { AuthContext } from "../provider/AuthProvider/";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = use(AuthContext);
+  const location = useLocation()
+//   console.log(location)
+//   console.log(user);
+
+if(loading) {
+    return <div className="flex justify-center items-center h-screen ">
+       <span  className="loading loading-spinner loading-xl"></span>
+    </div>
+}
+
+  if (user && user?.email) {
+    return children;
+  }
+  return <Navigate state={location.pathname} to="/auth/login"></Navigate>;
+};
+
+export default PrivateRoute;
